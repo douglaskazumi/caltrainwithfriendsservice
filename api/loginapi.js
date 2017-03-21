fc153c22364af788020dd254471dd9ac84c81666
Douglas Kazumi - Vendor <DKazumi@walmartlabs.com>
2017-03-20 18:01:25 -0700

    Data import

--- api/loginapi.js
+++ api/loginapi.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var util = require("../util");
+var fs = require('fs');
+var pg = require('pg');
+var copyFrom = require('pg-copy-streams').from;
 
 module.exports = {
   login: login
@@ -8,16 +11,24 @@ module.exports = {
 
 function login(req, res) {
 
-  var user = req.body.payload;
-  util.prettyPrint(user.loginId, "request");
+  // var user = req.body.payload;
+  // util.prettyPrint(user.loginId, "request");
   
-  var session = req.session;
-  util.prettyPrint(session, "session");
+  // var session = req.session;
+  // util.prettyPrint(session, "session");
 
-  res.json({
-    payload: {
-      authenticationState: "SUCCESS",
-      firstName: "User"
-    }
+  // res.json({
+  //   payload: {
+  //     authenticationState: "SUCCESS",
+  //     firstName: "User"
+  //   }
+  // });
+
+  pg.connect(CONNECT_STR, function(err, client, done) {
+    var stream = client.query(copyFrom('COPY agency FROM STDIN'));
+    var fileStream = fs.createReadStream('agency.txt')
+    fileStream.on('error', done);
+    fileStream.pipe(stream).on('finish', done).on('error', done);
   });
+
 }
