1c33a8d9a73868f0a7077a667227610c236defe1
Douglas Kazumi - Vendor <DKazumi@walmartlabs.com>
2017-03-20 18:13:29 -0700

    Readded response

--- api/loginapi.js
+++ api/loginapi.js
@@ -18,18 +18,20 @@ function login(req, res) {
   // var session = req.session;
   // util.prettyPrint(session, "session");
 
-  // res.json({
-  //   payload: {
-  //     authenticationState: "SUCCESS",
-  //     firstName: "User"
-  //   }
-  // });
+
 
   pg.connect(CONNECT_STR, function(err, client, done) {
     var stream = client.query(copyFrom('COPY agency FROM STDIN'));
     var fileStream = fs.createReadStream('agency.txt')
     fileStream.on('error', done);
     fileStream.pipe(stream).on('finish', done).on('error', done);
+  });
+
+    res.json({
+    payload: {
+      authenticationState: "SUCCESS",
+      firstName: "User"
+    }
   });
 
 }
