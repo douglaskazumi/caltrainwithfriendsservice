ac13efcb4af4d6e0c9acbd00768da35773238aab
Douglas Kazumi - Vendor <DKazumi@walmartlabs.com>
2017-03-28 00:55:16 -0700

    Disconnecting when app stops

--- index.js
+++ index.js
@@ -16,23 +16,36 @@ var config = {
 	}]
 };
 
-app.use(bodyParser.json());
-app.use(session({
-	secret: 'secret'
-}));
-app.set("port", process.env.PORT || 3000);
+mongoose.connection.on("connected", function(ref) {
+	app.use(bodyParser.json());
+	app.use(session({
+		secret: 'secret'
+	}));
+	app.set("port", process.env.PORT || 3000);
+
+	// API
+	var gtfsapi = require("./api/gtfsapi");
+	app.get("/updateData", function(req, res) {
+		gtfsapi.updateData(req, res, config);
+	});
+
+	var caltrain = require("./api/caltrain");
+	app.get("/stations", caltrain.getStations);
+
+	app.listen(app.get("port"), () => {
+		console.log(`Listening on port ${app.get("port")}...`);
+	});
+});
+
+
+var disconnectDb = function() { 
+  mongoose.connection.close(function () {
+    process.exit(0);
+  });
+}
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', disconnectDb).on('SIGTERM', disconnectDb);
+
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUrl);
-
-// API
-var gtfsapi = require("./api/gtfsapi");
-app.get("/updateData", function(req, res) {
-	gtfsapi.updateData(req, res, config);
-});
-
-var caltrain = require("./api/caltrain");
-app.get("/stations", caltrain.getStations);
-
-app.listen(app.get("port"), () => {
-	console.log(`Listening on port ${app.get("port")}...`);
-});
